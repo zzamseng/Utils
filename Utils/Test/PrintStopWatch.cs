e720fe3bf65ff5f63de7bf1e4981cbbebfb26547
zzamseng <zzamseng@gmail.com>
2019-08-22 23:21:10 +0900

    Init commit

---
+++ Utils/Test/PrintStopWatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Test
+{
+    public class PrintStopWatch
+    {
+        public static void Start<T>(Func<T> func)
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            func();
+            stopwatch.Stop();
+            Console.WriteLine($"Funtion elapsed time: {stopwatch.ElapsedMilliseconds.ToString()}");
+        }
+    }
+}
